ef63d688ce7ee2ced39a833e1f2c9d3ee1b29c32
johanhoareau <hoareaujohan.reactdev@gmail.com>
2025-05-26 13:43:28 +0400

    chore: add peerDependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
-import { fixupConfigRules } from '@eslint/compat'
-import { FlatCompat } from '@eslint/eslintrc'
-import js from '@eslint/js'
-import prettier from 'eslint-plugin-prettier'
-import { defineConfig } from 'eslint/config'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import babelParser from '@babel/eslint-parser'
+import { fixupConfigRules } from "@eslint/compat"
+import { FlatCompat } from "@eslint/eslintrc"
+import js from "@eslint/js"
+import prettier from "eslint-plugin-prettier"
+import { defineConfig } from "eslint/config"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import babelParser from "@babel/eslint-parser"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -17,22 +17,22 @@ const compat = new FlatCompat({
 
 export default defineConfig([
 	{
-		extends: fixupConfigRules(compat.extends('@react-native', 'prettier')),
+		extends: fixupConfigRules(compat.extends("@react-native", "prettier")),
 		plugins: { prettier },
 		languageOptions: {
 			parser: babelParser,
 		},
 		rules: {
-			'react/react-in-jsx-scope': 'off',
-			'indent': ['error', 'tab'],
-			'no-tabs': 'off',
-			'prettier/prettier': [
-				'error',
+			"react/react-in-jsx-scope": "off",
+			"indent": ["error", "tab"],
+			"no-tabs": "off",
+			"prettier/prettier": [
+				"error",
 				{
 					semi: false,
-					quoteProps: 'consistent',
-					singleQuote: true,
-					trailingComma: 'es5',
+					quoteProps: "consistent",
+					singleQuote: false,
+					trailingComma: "es5",
 					useTabs: true,
 					singleAttributePerLine: true,
 				},
@@ -40,6 +40,6 @@ export default defineConfig([
 		},
 	},
 	{
-		ignores: ['node_modules/', 'lib/'],
+		ignores: ["node_modules/", "lib/"],
 	},
 ])
