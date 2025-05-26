ff799614015ff521e72d553a49fe58c0ca6f59d3
johanhoareau <hoareaujohan.reactdev@gmail.com>
2025-05-26 10:05:03 +0400

    chore: update config eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,41 +1,45 @@
-import { fixupConfigRules } from '@eslint/compat';
-import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
-import prettier from 'eslint-plugin-prettier';
-import { defineConfig } from 'eslint/config';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { fixupConfigRules } from '@eslint/compat'
+import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
+import prettier from 'eslint-plugin-prettier'
+import { defineConfig } from 'eslint/config'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import babelParser from '@babel/eslint-parser'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
+})
 
 export default defineConfig([
-  {
-    extends: fixupConfigRules(compat.extends('@react-native', 'prettier')),
-    plugins: { prettier },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      'prettier/prettier': [
-        'error',
-        {
-          quoteProps: 'consistent',
-          singleQuote: true,
-          tabWidth: 2,
-          trailingComma: 'es5',
-          useTabs: false,
-        },
-      ],
-    },
-  },
-  {
-    ignores: [
-      'node_modules/',
-      'lib/'
-    ],
-  },
-]);
+	{
+		extends: fixupConfigRules(compat.extends('@react-native', 'prettier')),
+		plugins: { prettier },
+		languageOptions: {
+			parser: babelParser,
+		},
+		rules: {
+			'react/react-in-jsx-scope': 'off',
+			'indent': ['error', 'tab'],
+			'no-tabs': 'off',
+			'prettier/prettier': [
+				'error',
+				{
+					semi: false,
+					quoteProps: 'consistent',
+					singleQuote: true,
+					trailingComma: 'es5',
+					useTabs: true,
+					singleAttributePerLine: true,
+				},
+			],
+		},
+	},
+	{
+		ignores: ['node_modules/', 'lib/'],
+	},
+])
