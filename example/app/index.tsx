80d3958ca41aa51f8e460c7b2df2446d10cc60e3
johanhoareau <hoareaujohan.reactdev@gmail.com>
2025-06-24 12:10:12 +0400

    feat(example): add example

--- example/app/index.tsx
+++ example/app/index.tsx
@@ -1,12 +1,104 @@
-import { StyleSheet, Text, View } from "react-native"
-import React from "react"
+import { Button, StyleSheet, Text, View } from "react-native"
+import React, { useState } from "react"
+import {
+	useDelightfulTransition,
+	XScreen,
+	XView,
+} from "react-native-delightful-navigation"
 
 export default function App() {
+	const { register } = useDelightfulTransition("/")
+
+	const handlePress = () => {
+		console.log(JSON.stringify(register.registerRef.current, null, 2))
+	}
+
 	return (
-		<View>
-			<Text>App</Text>
-		</View>
+		<XScreen {...register}>
+			<XView
+				{...register}
+				tag="Page"
+				style={styles.xView}
+			>
+				<View>
+					<View>
+						<XView
+							{...register}
+							tag="Header"
+							style={styles.xView}
+						>
+							<XView
+								{...register}
+								tag="Nav"
+								style={styles.xView}
+							>
+								<Text>Test nav</Text>
+							</XView>
+						</XView>
+					</View>
+				</View>
+				<XView
+					{...register}
+					tag="Main"
+					style={styles.xView}
+				>
+					<XView
+						{...register}
+						tag="Sidebar"
+						style={styles.xView}
+					>
+						<XView
+							{...register}
+							tag="Menu 1"
+							style={styles.xView}
+						/>
+						<XView
+							{...register}
+							tag="Menu 2"
+							style={styles.xView}
+						/>
+					</XView>
+					<XView
+						{...register}
+						tag="Content"
+						style={styles.xView}
+					>
+						<XView
+							{...register}
+							tag="Article 1"
+							style={styles.xView}
+						/>
+						<XView
+							{...register}
+							tag="Article 2"
+							style={styles.xView}
+						/>
+					</XView>
+				</XView>
+				<XView
+					{...register}
+					tag="Footer"
+					style={styles.xView}
+				/>
+			</XView>
+			<View style={styles.buttonNavigation}>
+				<Button
+					title="Navigate"
+					onPress={handlePress}
+				/>
+			</View>
+		</XScreen>
 	)
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	xView: {
+		borderWidth: 1,
+		padding: 9,
+		gap: 20,
+		flex: 1,
+	},
+	buttonNavigation: {
+		marginBottom: 50,
+	},
+})
