ce8789866e7db1f91e42c35a779989ce0a804877
johanhoareau <hoareaujohan.reactdev@gmail.com>
2025-07-04 16:20:06 +0400

    fix: export XText

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 export { FlowManager } from "./components/FlowManager"
 export { XScreen } from "./components/XScreen"
-export { XView } from "./components/XComponents"
+export { XView, XText } from "./components/XComponents"
 export { useDelightfulTransition } from "./hooks/useDelightfulTransition"
